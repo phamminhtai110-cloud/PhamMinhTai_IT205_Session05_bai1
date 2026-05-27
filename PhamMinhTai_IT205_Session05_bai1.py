fb2df4adf9689a4b54fc3ee364bacc5ba4d4b0a4
phamminhtai110-cloud <phamminhtai110@gmail.com>
2026-05-27 21:40:56 +0700

    Add files via upload

---
+++ PhamMinhTai_IT205_Session05_bai1.py
@@ -0,0 +1,12 @@
+branch_count = int(input("Nhập số lượng chi nhánh: "))
+month_count = 3
+
+result = ""
+
+for branch in range(1, branch_count + 1):
+    for month in range(1, month_count + 1):
+        revenue = int(input(f"Nhập doanh thu Chi nhánh {branch}, tháng {month}: "))
+        result = result + f"Chi nhánh {branch}, tháng {month}: {revenue} triệu đồng\n"
+
+print("\n-------------- Kết quả --------------")
+print(result)
